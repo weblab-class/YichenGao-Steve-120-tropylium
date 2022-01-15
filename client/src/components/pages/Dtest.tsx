2fd3a715b21500e309cf41a5bce37e1524c6fbd4
tropylium <adg4096@gmail.com>
2022-01-15 18:20:39 -0500

    fix zoom in/ out functionality d3

--- client/src/components/pages/Dtest.tsx
+++ client/src/components/pages/Dtest.tsx
@@ -10,60 +10,79 @@ type d3testProp = RouteComponentProps & {
 
 const Dtest = (props: d3testProp) => {
     const graph_container = useRef(null);
-    const [graph, setGraph] = useState(null);
+    const width: number = 800;
+    const height: number = 800;
+    const center_x: number = width/2;
+    const center_y: number = height/2;
     let zoom;
 
     useEffect(() => {
-        drawCanvas();
-    });
+        //zoom = d3.zoom();
+        // const graph = d3
+        //     .select(graph_container.current)
+        //     .append("svg")
+        //     .attr("width", 800)
+        //     .attr("height", 800)
+        //     .call(zoom.on("zoom", function (event) {
+        //         graph.attr("transform", event.transform)
+        //      }))
+        //      .append("g")
+        //     ;
+        const selection = d3.select(graph_container.current);
 
-    function drawCanvas(): void {
-        zoom = d3.zoom();
-        const graph = d3
-            .select(graph_container.current)
-            .append("svg")
-            .attr("width", 800)
-            .attr("height", 800)
-            .call(zoom.on("zoom", function (event) {
-                graph.attr("transform", event.transform)
-             }))
-            .append("g")
-            ;
+        const svg = selection.append("svg")
+            .attr("width", width)
+            .attr("height", height);
         
-        // function handleZoom(event): void {
-        //     graph.attr('transform', event.transform);
-        // }
-        // let zoom = d3.zoom().on("zoom", handleZoom);
-        // graph.call(zoom);
+        const g = svg.append("g");
 
-        const center_x = 400;
-        const center_y = 400;
+        zoom = d3.zoom().on("zoom", function (event) {
+            g.attr("transform", event.transform)
+        });
+
+        selection.call(zoom);
+
 
         // background
-        graph.append("rect")
+        g.append("rect")
             .attr("width", "100%")
             .attr("height", "100%")
             .attr("fill", "white");
         
-        graph.append('circle')
+        g.append('circle')
         .attr('cx', center_x)
         .attr('cy', center_y)
         .attr('r', 50)
         .attr('stroke', 'black')
         .attr('fill', '#69a3b2');
-    }
+    });
 
     const resetGraph= (event) => {
-        d3.select('svg')
+        d3.select(graph_container.current)
 		.transition()
-		// .call(zoom.scaleTo, 1);
-        .call(zoom.transform, d3.zoomIdentity.translate(0,0).scale(1))
+        .call(zoom.transform, d3.zoomIdentity);
+    }
+
+    const zoomOut = (event) => {
+        d3.select(graph_container.current)
+		.transition()
+        .call(zoom.scaleBy, 0.5, [center_x,center_y]);
+    }
+
+    const zoomIn = (event) => {
+        d3.select(graph_container.current)
+		.transition()
+        .call(zoom.scaleBy, 2, [center_x,center_y]);
     }
 
     return (
     <div className="dtest-container">
         <div ref = {graph_container} className="dtest-graph-container"></div>
-        <div className="reset-button" onClick={resetGraph}>Reset</div>
+        <div className="dtest-button-container">
+            <div className="dtest-button" onClick={resetGraph}>Reset</div>
+            <div className="dtest-button" onClick={zoomOut}>Zoom Out</div>
+            <div className="dtest-button" onClick={zoomIn}>Zoom In</div>
+        </div>
     </div>);
 }
 
