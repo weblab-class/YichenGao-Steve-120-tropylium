e5e1546918e3dd0425962ce0b2d68c14ebbd60be
YichenGao <laliweis@gmail.com>
2022-01-15 13:04:42 -0500

    home page

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,31 +1,53 @@
 import React from "react";
 import "./Home.css";
 import { RouteComponentProps } from "@reach/router";
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 type HomeProps = RouteComponentProps;
 
-const Home = (props: HomeProps) => {
+//const Home = (props: HomeProps) => {
+  const Home: React.FC = () => {
   return (
-    <div className="home-container">
-      <h1>Fractory will win- Alex Dang Yichen Gao Steven Reyes</h1>
-      <h2> What we provide in this skeleton</h2>
-      <ul>
-        <li>Google Auth (Skeleton.js & auth.js)</li>
-        <li>Socket Infrastructure (client-socket.js & server-socket.js)</li>
-        <li>User Model (auth.js & user.js)</li>
-      </ul>
-      <h2> What you need to change</h2>
-      <ul>
-        <li>Change the font in utilities.css</li>
-        <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-        <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
-        <li>Change the Database SRV for Atlas (server.js)</li>
-        <li>Change the Database Name for MongoDB (server.js)</li>
-        <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-        <li>Update website title in client/dist/index.html</li>
-      </ul>
-    </div>
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
+
 
 export default Home;
