95a2c14ffef853af16b2b8fe51674cf362f63a57
YichenGao <laliweis@gmail.com>
2022-01-16 16:43:00 -0500

    fancy homepage feature

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -5,24 +5,12 @@ import Carousel from 'react-bootstrap/Carousel';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import logo_colored from "../../images/colored_name_logo.png";
+import logo_blue from "../../images/blue_name_logo.png";
+import logo_red from "../../images/red_name_logo.png";
 import fractal_example1 from "../../images/fractal_example1.png";
-import fractal_example2 from "../../images/fractal_example1.png";
-
-// import GoogleLogin, {
-//   GoogleLoginResponse,
-//   GoogleLoginResponseOffline,
-//   GoogleLogout,
-// } from "react-google-login";
-// import { Link } from "@reach/router";
-// const GOOGLE_CLIENT_ID = "345260256672-4hgaortvbdeb0t3mb8iuphdvehej9m14.apps.googleusercontent.com";
+import fractal_example2 from "../../images/fractal_example2.png";
 
 type HomeProps = RouteComponentProps;
-// type NavBarProps = {
-//   userId: String;
-//   handleLogin: (res: GoogleLoginResponse | GoogleLoginResponseOffline) => void;
-//   handleLogout: () => void;
-// };
 
 const Home = (props: HomeProps) => {
   return (
@@ -37,25 +25,12 @@ const Home = (props: HomeProps) => {
                               alt="First slide"
                           />
                           <Carousel.Caption>
-                              <img className="navbar-logo-icon" src={logo_colored} />
+                            <div className="centerImage">
+                              <img className="navbar-logo-icon" height="240" width="700" src={logo_blue} />
                               <h3>Create Your Fractal Art in just a few steps.</h3>
-                              <p>Log in to start your journey to imaginamtion.</p>
+                              <p>Log in to turn your imaginamtion into reality.</p>
+                            </div>
 
-                              {/* {props.userId ? (
-                                <GoogleLogout
-                                  clientId={GOOGLE_CLIENT_ID}
-                                  buttonText="Logout"
-                                  onLogoutSuccess={props.handleLogout}
-                                  onFailure={() => console.log(`Failed to logout.`)}
-                                />
-                              ) : (
-                                <GoogleLogin
-                                  clientId={GOOGLE_CLIENT_ID}
-                                  buttonText="Login"
-                                  onSuccess={props.handleLogin}
-                                  onFailure={(err) => console.log(err)}
-                                />
-                              )} */}
                           </Carousel.Caption>
                       </Carousel.Item>
                       <Carousel.Item>
@@ -66,24 +41,14 @@ const Home = (props: HomeProps) => {
                           />
 
                           <Carousel.Caption>
-                              <img className="navbar-logo-icon" src={logo_colored} />
+                          <div className="centerImage">
+                              <img className="navbar-logo-icon" height="240" width="700" src={logo_red} />
                               <h3>Create Your Fractal Art in just a few steps.</h3>
-                              <p>Log in to start your journey to imaginamtion.</p>
+                              <p>Log in to turn your imaginamtion into reality.</p>
+                            </div>
                           </Carousel.Caption>
                       </Carousel.Item>
-                      <Carousel.Item>
-                          <img
-                              className="d-block w-100"
-                              src={fractal_example1}
-                              alt="Third slide"
-                          />
-
-                          <Carousel.Caption>
-                              <img className="navbar-logo-icon" src={logo_colored} />
-                              <h3>Create Your Fractal Art in just a few steps.</h3>
-                              <p>Log in to start your journey to imaginamtion.</p>
-                          </Carousel.Caption>
-                      </Carousel.Item>
+                      
                   </Carousel>
               </Col>
           </Row>
